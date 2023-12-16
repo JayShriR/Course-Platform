666e29313eb2e4029b39514fad2f2d8a4f5988e0
JayShriR <navrangerameshwar@gmail.com>
2023-12-16 22:15:27 +0530

    modified 2

--- course/src/App.js
+++ course/src/App.js
@@ -1,25 +1,8 @@
-import { useEffect, useState } from 'react';
-
-function App() {
-  const [text, setText] = useState('');
- 
-
-  useEffect(() => {
-    
-      console.log("ui rendering");
-     
-  }, [text]); // Empty dependency array ensures this effect runs only once on initial mount
-
-  function changeHandler(event) {
-    setText(event.target.value);
-    console.log(text);
-  }
-
-  return (
-    <div className="App">
-      <input type="text" onChange={changeHandler} />
-    </div>
-  );
-}
+import react from "react";
+const App=()=>{
+    return (<div>
+        App
+    </div>)
+};
 
 export default App;
