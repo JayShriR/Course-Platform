03cdc12735647c539dd8ceeaca25c3e0c3c844f5
JayShriR <navrangerameshwar@gmail.com>
2023-12-17 20:22:05 +0530

    modified file 3

--- course/src/App.js
+++ course/src/App.js
@@ -1,8 +1,30 @@
-import react from "react";
-const App=()=>{
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Filter from "./components/FIlter";
+import Cards from "./components/Cards";
+import { apiUrl, filterData } from "./data";
+import { toast } from "react-toastify";
+
+    const App=()=>{
+        const [courses,setCourses]=useState();
+        useEffect( () => {
+            const fetchdata = async() => {
+                try{
+                    const res= await fetch(apiUrl);
+                    const output= res.json;
+                    console.log(output.data);
+                }
+                catch(error){
+                    toast.error("something went wrong");
+                }
+            }
+            fetchdata();
+        },[])
     return (<div>
         <Navbar/>
-        <Filter/>
+        <Filter
+            filterData={filterData}
+        />
         <Cards/>
     </div>)
 };
---
+++ course/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+const Nav=()=>{
+    return(
+        <div>
+            <nav>
+                <h1>
+                    TOP COURSES
+                </h1>
+            </nav>
+        </div>
+    )   
+}
+export default Nav;
